307c8aed2006da507808fbc00197e81707bd19e5
Ryan LaLiberty <rycolos@gkserver.net>
2023-07-29 17:14:37 -0400

    display script updates

--- ink.py
+++ ink.py
@@ -1,18 +1,47 @@
 import pandas as pd
-from papirus import Papirus
-from papirus import PapirusText
+import time
+#from papirus import Papirus
+#from papirus import PapirusTextPos
 
+refresh = 15   #refresh time
 
-#read last line of log file, handle newlines in data
-df = pd.read_csv('log-test.log').tail(1)
-df = df.replace(r'\n',' ', regex=True) 
+#every 30s, read csv, update display, don't update if no change
+try:
+    while True:
+        #read last line of log file, handle newlines in data
+        df = pd.read_csv('log-test.log').tail(1).replace(r'\n',' ', regex=True) 
+        last = df[['isotime', 'source', 'heard', 'symbol', 'level', 'latitude', 'longitude', 'comment']]
+        row_list = last.values.flatten().tolist()
+        isotime, source, heard, symbol, level, latitude, longitude, comment = row_list
 
-selection = df[['isotime', 'source', 'heard', 'symbol', 'level', 'latitude', 'longitude', 'comment']]
-row_list = selection.values.flatten().tolist()
+        #write to display
+        #screen.partial_update()
+        #text = PapirusTextPos([rotation = 0])
 
-print(row_list[0])
-print(*row_list[1:5])
-print(*row_list[5:7])
-print(row_list[7])
+        # r1 = row_list[0]
+        # r2 = row_list[1:5]
+        # r3 = row_list[5:7]
+        # r4 = row_list[7]
 
-screen.partial_update()
+        # # print(f"""
+        # # {row_list[0]}
+        # # {row_list[1:5]}
+        # # {row_list[5:7]}
+        # # {row_list[7]}
+        # #     """)
+
+        # # print(r1)
+        # # print(' '.join(map(str, r2)))
+        # # print(' '.join(map(str, r3)))
+        # # print(r4)
+
+        print(isotime)
+        print(source, heard, symbol, level)
+        print(latitude, longitude)
+        print(comment)
+
+        #wait for refresh time
+        time.sleep(refresh)
+    
+except KeyboardInterrupt:
+    pass
