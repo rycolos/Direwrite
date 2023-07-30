dddabfb5cd500b83cd6ab58f5b13c52a51843e3c
Ryan LaLiberty <rycolos@gkserver.net>
2023-07-30 07:19:22 -0400

    display script updates

--- ink.py
+++ ink.py
@@ -1,37 +1,49 @@
 import pandas as pd
 import time
-from papirus import PapirusText
+from datetime import date
 from papirus import PapirusTextPos
 
 refresh = 15   #refresh time
-file = 'log-test.log'
+logdir = '~/direwolf-logs'
+file1 = 'log-test.log'
+logfile = f"{logdir}/{date.today}.log"
 
+#initialize Papirus
 text = PapirusTextPos()
 text.Clear()
+
+"""
+need to initialize logfile to try to read it
+if it doesn't exist, write "Waiting for logs to populate..."
+"""
+
 
 #every 30s, read csv, update display, don't update if no change
 try:
     while True:
-        #read last line of log file, handle newlines in data
-        df = pd.read_csv(file).tail(1).replace(r'\n',' ', regex=True) 
-        last = df[['isotime', 'source', 'heard', 'symbol', 'level', 'latitude', 'longitude', 'comment']]
-        row_list = last.values.flatten().tolist()
-        isotime, source, heard, symbol, level, latitude, longitude, comment = row_list
+     #read last line of log file, handle newlines in data
+        
+        try:
+            df = pd.read_csv(file1).tail(1).replace(r'\n',' ', regex=True) 
+            last = df[['isotime', 'source', 'heard', 'symbol', 'level', 'latitude', 'longitude', 'comment']]
+            row_list = last.values.flatten().tolist()
+            isotime, source, heard, symbol, level, latitude, longitude, comment = row_list
 
         #write to display
-        msg = (f"{isotime}\n"
-          f"{source}, {heard}, {symbol}, {level}\n"
-          f"{latitude}, {longitude}\n"
-          f"{comment}\n"
-         )
-        print(msg)
+            msg = (f"{isotime}\n"
+            f"{source}, {heard}, {symbol}, {level}\n"
+            f"{latitude}, {longitude}\n"
+            f"{comment}\n"
+            )
+            print(msg)
+            text.AddText(msg, 0, 0, Id="decode", size=15)
         
-        #text.AddText(f"{isotime}\n{source} {heard} {symbol} {level}\n{latitude} {longitude}\n{comment}",
-       #     0, 0, Id="row1", size=15)
-        text.AddText(msg, 0, 0, Id="row1", size=15)
+        except:
+            text.AddText("Waiting for logs to populate", 0, 0, Id="error", size=15)
 
-        #wait for refresh time
-        time.sleep(refresh)
+    #wait for refresh time
+        finally:
+            time.sleep(refresh)
     
 except KeyboardInterrupt:
     pass
