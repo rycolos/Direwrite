2194253f62d996e4aba92a5a56e97e3279ca415e
Ryan LaLiberty <rycolos@gkserver.net>
2023-07-30 07:02:12 -0400

    display script updates

--- ink.py
+++ ink.py
@@ -41,8 +41,14 @@ try:
         
         rot = 0 
         text = PapirusTextPos()
-        text.AddText(f"{isotime}\n{source} {heard} {symbol} {level}\n{latitude} {longitude}\n{comment}",0, 0, Id="row1", size=15)
-        #screen.partial_update()
+        #text.AddText(f"{isotime}\n{source} {heard} {symbol} {level}\n{latitude} {longitude}\n{comment}", 0, 0, Id="row1", size=15)
+        text.AddText(f"""
+            {isotime}
+            {source} {heard} {symbol} {level}
+            {latitude} {longitude}
+            {comment}"""
+        , 0, 0, Id="row1", size=15)
+
 
         #wait for refresh time
         time.sleep(refresh)
