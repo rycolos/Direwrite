ee94cd3be88f084bf654ea4a7b88dedcf7aab4a4
Ryan LaLiberty <rycolos@gkserver.net>
2023-07-31 14:58:05 -0400

    display script updates

--- direwrite.py
+++ direwrite.py
@@ -24,25 +24,19 @@ try:
      
      #read last line of log file, handle new lines in data   
         try:
-            #print(f"Reading {logfile}...")
             df_curr = pd.read_csv(logfile, encoding = "ISO-8859-1").tail(1).replace(r'\n',' ', regex=True)
-            if df_curr.equals(df_prev): #don't rewrite if not needed
-                print('no change needed')
-                continue
-            else:
-                print('writing new message')
-                cols = df_curr[['isotime', 'source', 'heard', 'symbol', 'level', 'latitude', 'longitude', 'comment']]
-                row_list = cols.values.flatten().tolist()
-                isotime, source, heard, symbol, level, latitude, longitude, comment = row_list
+            print('writing new message')
+            cols = df_curr[['isotime', 'source', 'heard', 'symbol', 'level', 'latitude', 'longitude', 'comment']]
+            row_list = cols.values.flatten().tolist()
+            isotime, source, heard, symbol, level, latitude, longitude, comment = row_list
 
-            #write to display
-                msg = (f"{isotime}\n"
-                    f"{source}, {heard}, {symbol}, {level}\n"
-                    f"{latitude}, {longitude}\n"
-                    f"{comment}\n"
-                    )
-                text.UpdateText("message", msg)
-            df_prev = df_curr
+        #write to display
+            msg = (f"{isotime}\n"
+                f"{source}, {heard}, {symbol}, {level}\n"
+                f"{latitude}, {longitude}\n"
+                f"{comment}\n"
+                )
+            text.UpdateText("message", msg)
     
     #show error if no logs yet
         except:
